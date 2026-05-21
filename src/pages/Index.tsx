28bd0d97a73d597e8e9e824ac899ac608e9ffdad
Skip <skip@usecurling.com>
2026-05-21 22:46:13 +0000

    feat: Add commercial section with diagnostic CTA

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from '@/components/sections/Hero'
 import { ServicesSection } from '@/components/sections/Services'
 import { AboutSection } from '@/components/sections/About'
+import { CommercialCTA } from '@/components/sections/CommercialCTA'
 import { ContactSection } from '@/components/sections/Contact'
 
 const Index = () => {
@@ -9,6 +10,7 @@ const Index = () => {
       <HeroSection />
       <ServicesSection />
       <AboutSection />
+      <CommercialCTA />
       <ContactSection />
     </div>
   )
