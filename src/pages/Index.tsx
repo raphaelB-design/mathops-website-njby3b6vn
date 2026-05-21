35c1dbda30b03d38eb860736b758c7bbee349011
Skip <skip@usecurling.com>
2026-05-21 22:52:15 +0000

    feat: Add preliminary diagnostic section and CTA

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from '@/components/sections/Hero'
 import { ServicesSection } from '@/components/sections/Services'
 import { AboutSection } from '@/components/sections/About'
+import { DiagnosticCTA } from '@/components/sections/DiagnosticCTA'
 import { CommercialCTA } from '@/components/sections/CommercialCTA'
 import { ContactSection } from '@/components/sections/Contact'
 
@@ -10,6 +11,7 @@ const Index = () => {
       <HeroSection />
       <ServicesSection />
       <AboutSection />
+      <DiagnosticCTA />
       <CommercialCTA />
       <ContactSection />
     </div>
